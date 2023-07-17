00ff9235d5a75a6e26b8e67409ab79c7f21c3aeb
Dominik Krejcik <dominik.krejcik@gmail.com>
2023-07-17 17:01:12 +0100

    build: add rome linter and formatter

--- src/web-components.ts
+++ src/web-components.ts
@@ -1,18 +1,18 @@
 export function injectComponentScripts() {
-    injectScriptOnce(makeLearnMoreScript());
+  injectScriptOnce(makeLearnMoreScript());
 }
 
 function injectScriptOnce(el: HTMLScriptElement) {
-    if (!document.getElementById(el.id)) {
-        document.head.append(el);
-    }
+  if (!document.getElementById(el.id)) {
+    document.head.append(el);
+  }
 }
 
 function makeLearnMoreScript() {
-    const learnMoreScript = document.createElement('script');
-    learnMoreScript.id = 'pd-learn-more-script';
-    learnMoreScript.src = 'https://www.purpledotprice.com/api/v1/learn-more.js';
-    learnMoreScript.async = true;
-    learnMoreScript.defer = true;
-    return learnMoreScript;
+  const learnMoreScript = document.createElement("script");
+  learnMoreScript.id = "pd-learn-more-script";
+  learnMoreScript.src = "https://www.purpledotprice.com/api/v1/learn-more.js";
+  learnMoreScript.async = true;
+  learnMoreScript.defer = true;
+  return learnMoreScript;
 }

38dcafebef5d0aa6347992bbab1cf64a3fe7c05f
Dominik Krejcik <dominik.krejcik@gmail.com>
2023-07-17 16:45:56 +0100

    feat: inject learn more web component script on init (#1)
    * feat: init npm package
    
    * feat: inject learn more web component script on init
    
    * build: add circleci config
    
    * build: use @tsconfig/recommended

---
+++ src/web-components.ts
@@ -0,0 +1,18 @@
+export function injectComponentScripts() {
+    injectScriptOnce(makeLearnMoreScript());
+}
+
+function injectScriptOnce(el: HTMLScriptElement) {
+    if (!document.getElementById(el.id)) {
+        document.head.append(el);
+    }
+}
+
+function makeLearnMoreScript() {
+    const learnMoreScript = document.createElement('script');
+    learnMoreScript.id = 'pd-learn-more-script';
+    learnMoreScript.src = 'https://www.purpledotprice.com/api/v1/learn-more.js';
+    learnMoreScript.async = true;
+    learnMoreScript.defer = true;
+    return learnMoreScript;
+}

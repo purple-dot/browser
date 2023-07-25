165b8baff801c31343651fcc502553b523e7a509
Dominik Krejcik <dominik.krejcik@gmail.com>
2023-07-25 17:47:24 +0100

    refactor: switch to multiple entry points

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
 import { injectComponentScripts } from "./web-components";
 import { trackPageView } from "./tracking";
 import { onDOMContentLoaded, onLocationChange } from "./custom-events";
-import * as checkout from "./checkout";
-import * as cart from "./cart";
-import * as api from "./api";
-import { ShopifyAJAXCart } from "./shopify-cart";
-import * as ShopifyAJAXCartInterceptors from "./interceptors";
 
 export interface PurpleDotConfig {
   apiKey: string;
@@ -21,5 +16,3 @@ export function init(config: PurpleDotConfig) {
 
 onDOMContentLoaded(() => trackPageView().catch(() => {}));
 onLocationChange(() => trackPageView().catch(() => {}));
-
-export { cart, checkout, api, ShopifyAJAXCart, ShopifyAJAXCartInterceptors };

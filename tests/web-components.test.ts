5d099bf55187ac362e1fbadd4c316673db9f0dcd
Dominik Krejcik <dominik.krejcik@gmail.com>
2023-07-26 17:12:37 +0100

    feat: wait for checkout script to load in checkout.open()

---
+++ tests/web-components.test.ts
@@ -0,0 +1,37 @@
+// @vitest-environment happy-dom
+
+import { describe, expect, test, vi } from "vitest";
+import { waitFor } from "@testing-library/dom";
+
+import {
+  injectComponentScripts,
+  onceCheckoutScriptLoaded,
+} from "../src/web-components";
+
+describe("onceCheckoutScriptLoaded", () => {
+  test("fires the callback once the script is loaded", async () => {
+    const callback = vi.fn();
+    onceCheckoutScriptLoaded(callback);
+
+    injectComponentScripts();
+
+    return waitFor(() => {
+      expect(callback).toHaveBeenCalled();
+    });
+  });
+
+  test("fires the callback immediately if the script is already loaded", async () => {
+    const callback = vi.fn();
+    onceCheckoutScriptLoaded(callback);
+
+    injectComponentScripts();
+
+    await waitFor(() => {
+      expect(callback).toHaveBeenCalled();
+    });
+
+    const anotherCallback = vi.fn();
+    onceCheckoutScriptLoaded(anotherCallback);
+    expect(anotherCallback).toHaveBeenCalled();
+  });
+});

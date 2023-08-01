df9efec8a803b57d6081ced4b61b4ae5a78f2c69
Dominik Krejcik <dominik.krejcik@gmail.com>
2023-08-01 16:31:11 +0100

    fix: only open checkout once

--- src/checkout.ts
+++ src/checkout.ts
@@ -2,6 +2,10 @@ import { onceCheckoutScriptLoaded } from "./web-components";
 import { getCartAdapter } from "./cart";
 
 export async function open(args?: { cartId?: string }) {
+  if (document.querySelector("purple-dot-checkout")) {
+    return;
+  }
+
   const element = document.createElement("purple-dot-checkout");
   document.body.appendChild(element);
 

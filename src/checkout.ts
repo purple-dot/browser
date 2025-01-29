a64fa2bd5c7e7ecda28c1a117bc29eac0ef9ab3b
Surya Rajendhran <32726562+suryarajendhran@users.noreply.github.com>
2025-01-29 12:09:05 +0530

    feat: add support for express checkout (#32)
    Allows the shopper to checkout with a single variant using the express
    checkout.

--- src/checkout.ts
+++ src/checkout.ts
@@ -20,3 +20,27 @@ export async function open(args?: { cartId?: string }) {
     });
   });
 }
+
+export async function openExpressCheckout(args: {
+  variantId: string;
+  releaseId: string;
+  currency: string;
+  quantity: number;
+  templatePaymentPlanId?: string;
+}) {
+  if (document.querySelector("purple-dot-checkout")) {
+    return;
+  }
+
+  const element = document.createElement("purple-dot-checkout");
+  document.body.appendChild(element);
+
+  return new Promise<void>((resolve) => {
+    onceCheckoutScriptLoaded(async () => {
+      // @ts-ignore
+      element.expressCheckout(args);
+
+      resolve();
+    });
+  });
+}

fdae9ddd3c21cad01ab2ea79e3ff3173ae63fe7c
Alex Stapleton <alexs@prol.etari.at>
2025-07-01 14:34:05 +0100

    feat: new availability API (#47)
    New API for availability in the browser SDK.
    
    The current "preorder-state" API has some issues when working with
    brands like SFCC where we don't keep track of their non-preorder
    inventory. Specifically it will *never* return the correct state of IN
    STOCK. This is not a huge problem, Kendra for example have managed to
    mostly work around this themselves but it definitely made things more
    confusing for them and I had to point them in the right direction a bit.
    
    This approach allows us to provide a consistent availability API
    regardless of whether we have PD-side tracking of in stock inventory and
    also allows us to encourage the merchant gently to not call PD unless
    they actually need to. We achieve this by adding a hook which allows the
    merchant to inject their own in stock availability data whenever
    checking the preorder state.
    
    ```ts
    init({
      inStockAvailability: ({ variantId }) => fetch(`/kendras-products?variant=${variant}`).json().available === true
    });
    
    function MyButton({variantId: string}) {
      const [availability, setAvailability] = useState();
      
      useEffect(async () => {
        availability({ variantId }).then((a) => setAvailability(a));   
      }, [variantId]);
    
      // Render the correct state for the button.
    }
    
    ```

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,36 @@
+import type { PurpleDotAvailability } from "./availability";
 import type { Cart, CartItem } from "./cart";
 
+/**
+ * Global Purple Dot configuration.
+ */
 export interface PurpleDotConfig {
+	/**
+	 * The Purple Dot API key for the store.
+	 */
 	apiKey: string;
-	cartAdapter: Cart<CartItem>;
-}
 
+	/**
+	 * Optional cart adapter for working with the combined checkout.
+	 */
+	cartAdapter?: Cart<CartItem>;
+
+	/**
+	 * Optional default hook for checking the in stock availability of a product.
+	 */
+	inStockAvailability?: <I>(
+		request: { variantId: string } | { productHandle: string },
+		getPreorderState: () => Promise<PurpleDotAvailability | null>,
+	) => Promise<I | false>;
+}
 let config: PurpleDotConfig | null = null;
 
-export function setConfig(c: PurpleDotConfig) {
-	config = c;
+export function setConfig(newConfig: PurpleDotConfig) {
+	config = newConfig;
 
 	// For compatibility with checkout.js script that reads the config from window
 	if (globalThis.window) {
-		globalThis.window.PurpleDotConfig = c;
+		globalThis.window.PurpleDotConfig = newConfig;
 	}
 }
 
--- src/shopify-in-stock-availability.ts
+++ src/shopify-in-stock-availability.ts
@@ -1,9 +1,10 @@
 import type { ProductPreorderState } from "./api";
 
 export async function shopifyInStockAvailability(
-	preorderStatePromise: Promise<ProductPreorderState>,
+	_request: { variantId: string } | { productHandle: string },
+	getPreorderState: () => Promise<ProductPreorderState>,
 ) {
-	const preorderState = await preorderStatePromise;
+	const preorderState = await getPreorderState();
 
 	return preorderState.state === "AVAILABLE_IN_STOCK";
 }

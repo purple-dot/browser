e3bf7d5378934672e2b6b963775d37b00a44ad95
Tudor Sandulescu <7089284+tudddorrr@users.noreply.github.com>
2025-07-03 14:39:11 +0100

    feat: add types for checkout element

--- src/checkout.ts
+++ src/checkout.ts
@@ -1,6 +1,37 @@
 import { getCartAdapter } from "./cart";
 import { onceCheckoutScriptLoaded } from "./web-components";
 
+export type PurpleDotAddItemResponse =
+	| {
+			success: true;
+			error: null;
+	  }
+	| {
+			success: false;
+			error: {
+				message: string;
+				data: Record<string, unknown>;
+			};
+	  };
+
+export interface PurpleDotCheckoutElement extends Element {
+	open: (args: { cartId: string; cartType: string }) => void;
+	expressCheckout: (args: {
+		variantId: string;
+		releaseId: string;
+		currency: string;
+		quantity: number;
+		templatePaymentPlanId?: string;
+	}) => void;
+	addItem: (args: {
+		variantId: string;
+		releaseId: string;
+		currency: string;
+		quantity: number;
+		templatePaymentPlanId?: string;
+	}) => Promise<PurpleDotAddItemResponse>;
+}
+
 export async function open(args?: { cartId?: string }) {
 	if (document.querySelector("purple-dot-checkout")) {
 		return;
@@ -34,8 +65,20 @@ export async function openExpressCheckout(args: {
 		onceCheckoutScriptLoaded(async () => {
 			// @ts-ignore
 			element.expressCheckout(args);
-
 			resolve();
+		});
+	});
+}
+
+export async function purpleDotCheckout<T>(
+	cb: (element: PurpleDotCheckoutElement) => T,
+) {
+	const element = getOrCreateCheckoutElement();
+
+	return new Promise<T>((resolve) => {
+		onceCheckoutScriptLoaded(async () => {
+			const result = await cb(element);
+			resolve(result);
 		});
 	});
 }
@@ -48,5 +91,5 @@ function getOrCreateCheckoutElement() {
 		document.body.appendChild(element);
 	}
 
-	return element;
+	return element as PurpleDotCheckoutElement;
 }

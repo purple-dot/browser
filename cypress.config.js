73acd80a0d245a175c30ee485222555eec7bc094
Ryszard T. Kaleta <ryszardkaleta@gmail.com>
2025-08-14 11:49:56 +0100

    chore: remove unused cypress setup (#68)
    We've not been using it since
    https://github.com/purple-dot/browser/pull/46 and in a conversation
    today decided the test wasn't much use anyway.

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  e2e: {
-    chromeWebSecurity: false,
-    baseUrl: 'http://localhost:8080',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-};

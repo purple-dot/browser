527f9bcfb61ece411fc6e7e8d24ed434be99d49f
Leigh Makewell <leigh@getpurpledot.com>
2025-11-18 10:46:25 +0000

    fix: lint

--- tests/web-components.test.ts
+++ tests/web-components.test.ts
@@ -14,8 +14,8 @@ describe("onceCheckoutScriptLoaded", () => {
 		// biome-ignore lint/suspicious/noExplicitAny: Happy DOM types are not fully exposed
 		const windowWithHappyDOM = window as any;
 		if (windowWithHappyDOM.happyDOM?.settings) {
-      // Use of this setting is a potential security risk. If this ever evaluated untrusted code, it could lead to a security vulnerability.
-      // We enable this as we are verifying we can load scripts from our own domain.
+			// Use of this setting is a potential security risk. If this ever evaluated untrusted code, it could lead to a security vulnerability.
+			// We enable this as we are verifying we can load scripts from our own domain.
 			windowWithHappyDOM.happyDOM.settings.enableJavaScriptEvaluation = true;
 			windowWithHappyDOM.happyDOM.settings.disableJavaScriptFileLoading = false;
 		}
